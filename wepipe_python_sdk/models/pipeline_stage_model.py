27a8cdafed4ffd9b6619eeab9edcd7525a183faf
Marcus Stinghel <vini.o.stinghel@gmail.com>
2024-10-23 10:47:37 -0300

    UPDATE - pipeline_stage_model.py

--- wepipe_python_sdk/models/pipeline_stage_model.py
+++ wepipe_python_sdk/models/pipeline_stage_model.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from datetime import datetime
 
 
 class PipelineStageModel(BaseModel):
     stage: str = Field(default=None, alias='stage')
     stage_index: int = Field(default=None, alias='stage_index')
     total: Optional[int] = Field(default=None, alias='total')
-    moved_at: Optional[str] = Field(default=None, alias='moved_at')
+    moved_at: Optional[datetime] = Field(default=None, alias='moved_at')
